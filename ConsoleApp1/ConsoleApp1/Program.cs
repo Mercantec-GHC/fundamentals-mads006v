0a5ce37092663b7419b9a4ae768722e7af26e7fd
mads006v <mach0015@edu.mercantec.dk>
2024-10-22 12:49:28 +0200

    banko

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine(i+" bankebøf");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine(i + " banke");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine(i + " bøf");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
--- ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+642f53e7dfb51baaa3e8c485eff6303fc9d09478")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e2e2c9e25d598fb8ab448e2e642f1bf269cfa06")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
